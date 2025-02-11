3cac6498038bcf98b3be19be097fd1ce2e088dcd
kobibest <kobi@eitan-m.co.il>
2025-02-11 14:14:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { mime } from 'vite-plugin-mime';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mime()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
